d27ba8fe754d5fe3c81c7894dfe61b47edf9ca83
Logan Dallalio <logan@dallalioweb.dev>
2022-01-29 02:07:23 -0600

    setup Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+
 function App() {
 	return (
-		<div className='App'>
-			<h1>MyApp</h1>
-		</div>
+		<>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Explore />} />
+					<Route path='/offers' element={<Offers />} />
+					<Route path='/forgot-password' element={<ForgotPassword />} />
+					<Route path='/sign-in' element={<SignIn />} />
+					<Route path='/sign-up' element={<SignUp />} />
+					<Route path='/profile' element={<SignIn />} />
+				</Routes>
+			</Router>
+		</>
 	);
 }
 
---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ForgotPassowrd() {
+	return (
+		<div>
+			<h1>Forgot Passowrd</h1>
+		</div>
+	);
+}
+
+export default ForgotPassowrd;

431f726f07f7e9bd3073ffe91b67093482543794
Logan Dallalio <logan@dallalioweb.dev>
2022-02-13 15:28:00 -0600

    Adding reset Password via firebase

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 
 function ForgotPassowrd() {
+	const [email, setEmail] = useState('');
+	const onChange = (e) => {
+		setEmail(e.target.value);
+	};
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const auth = getAuth();
+			await sendPasswordResetEmail(auth, email);
+			toast.success('Email sent successfully');
+		} catch (error) {
+			toast.error('Email not sent');
+		}
+	};
 	return (
-		<div>
-			<h1>Forgot Passowrd</h1>
+		<div className='pageContainer'>
+			<header>
+				<p className='pageHeader'>ForgotPassword</p>
+			</header>
+			<main>
+				<form onSubmit={onSubmit}>
+					<input
+						type='email'
+						className='emailInput'
+						id='email'
+						value={email}
+						onChange={onChange}
+					/>
+					<Link className='forgotPasswordLink' to='/login'>
+						Sign In
+					</Link>
+					<div className='signInBar'>
+						<div className='signInText'>Send Reset Link</div>
+						<button className='signInButton'>
+							<ArrowRightIcon fill='#fffff' width='34px' height='34px' />
+						</button>
+					</div>
+				</form>
+			</main>
 		</div>
 	);
 }

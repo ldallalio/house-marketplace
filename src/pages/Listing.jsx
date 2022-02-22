3e9168ba90f17f3f7c09002d73d1e9139691a0e2
Logan Dallalio <logan@dallalioweb.dev>
2022-02-22 15:43:21 -0600

    Adding Lisiting Details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { getDoc, doc } from 'firebase/firestore';
@@ -29,7 +30,66 @@ function Listing() {
 		};
 		fetchListing();
 	}, [navigate, params.listingId]);
-	return <div>Listing</div>;
+
+	if (loading) {
+		return <Spinner />;
+	}
+	return (
+		<main>
+			{/* slider */}
+			<div
+				className='shareIconDiv'
+				onClick={() => {
+					navigator.clipboard.writeText(window.location.href);
+					setShareLinkCopied(true);
+					setTimeout(() => {
+						setShareLinkCopied(false);
+					}, 2000);
+				}}>
+				<img src={shareIcon} alt='share icon' className='shareIcon' />
+			</div>
+			{shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+			<div className='listingDetails'>
+				<p className='listingName'>
+					{listing.name} - $
+					{listing.offer ? listing.discountedPrice : listing.regularPrice}
+				</p>
+				<p className='listingLocation'>{listing.location}</p>
+				<p className='listingType'>
+					For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+				</p>
+				{listing.offer && (
+					<p className='discountPrice'>
+						${listing.regularPrice - listing.discountedPrice} Discount
+					</p>
+				)}
+				<ul className='listingDetailsList'>
+					<li>
+						{listing.bedrooms > 1
+							? `${listing.bedrooms} Bedrooms`
+							: `${listing.bedrooms} Bedroom`}
+					</li>
+					<li>
+						{listing.bathrooms > 1
+							? `${listing.bathrooms} Bathrooms`
+							: `${listing.bathrooms} Bathroom`}
+					</li>
+					<li>{listing.parking && 'Parking Spot'}</li>
+					<li>{listing.furnished && 'Furnished'}</li>
+				</ul>
+				<p className='listingLocationTitle'>Location</p>
+				{/* Map */}
+
+				{auth.currentUser?.uid !== listing.userRef && (
+					<Link
+						to={`/contact/${listing.userRef}?listingName=${listing.name} & listingLocation=${listing.location}`}
+						className='primaryButton'>
+						Contact Owner
+					</Link>
+				)}
+			</div>
+		</main>
+	);
 }
 
 export default Listing;
